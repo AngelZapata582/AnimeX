5b11510b18b33633684ddbc4540ed8cd3c365108
angel zapata <angelzapata582@gmail.com>
2022-01-11 23:30:19 -0600

    refactor: se importaron los componentes de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    MainPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
